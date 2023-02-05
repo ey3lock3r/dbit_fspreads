1e7e9d423b6a515f598292fc22f754708e1d8f94
Dhinson Dacpano <dhinson.dacpano@gmail.com>
2023-02-05 14:35:09 +0100

    fixes

--- Bot.py
+++ Bot.py
@@ -151,7 +151,7 @@ class CBot:
     def get_ord_size(self):
         ord_size = self.equity * self.risk_perc
         self.logger.info(f'ord_size {ord_size} = equity {self.equity} * risk perc {self.risk_perc}')
-        ord_size /= self.margin / 2 # 0.03? margin 2? pm acct
+        ord_size = ord_size / self.margin / 2 # 0.03? margin 2? pm acct
         self.logger.info(f'ord_size 1 = {ord_size}')
         ord_size *= self.fs_data[self.index_name]
         self.logger.info(f'ord_size 2 = {ord_size}')
